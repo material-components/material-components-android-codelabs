29e82e0eb6c48c1707f3b28c1589c31859b1564c
Cameron Ketcham <cketcham@google.com>
2018-04-19 16:39:53 -0400

    Starting point for 111

---
+++ java/shipping/app/src/main/java/com/google/codelabs/mdc/java/shipping/Utils.java
@@ -0,0 +1,32 @@
+package com.google.codelabs.mdc.java.shipping;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Utils class used in MDC-111 application.
+ */
+public abstract class Utils {
+
+    public static <T extends View> List<T> findViewsWithType(View root, Class<T> type) {
+        List<T> views = new ArrayList<>();
+        findViewsWithType(root, type, views);
+        return views;
+    }
+
+    private static <T extends View> void findViewsWithType(View view, Class<T> type, List<T> views) {
+        if (type.isInstance(view)) {
+            views.add(type.cast(view));
+        }
+
+        if (view instanceof ViewGroup) {
+            ViewGroup viewGroup = (ViewGroup) view;
+            for (int i = 0; i < viewGroup.getChildCount(); i++) {
+                findViewsWithType(viewGroup.getChildAt(i), type, views);
+            }
+        }
+    }
+}
---
+++ kotlin/shipping/app/src/main/java/com/google/codelabs/mdc/kotlin/shipping/Utils.kt
@@ -0,0 +1,30 @@
+package com.google.codelabs.mdc.kotlin.shipping
+
+import android.view.View
+import android.view.ViewGroup
+
+import java.util.ArrayList
+
+/**
+ * Utils class used in MDC-111 application.
+ */
+object Utils {
+
+    fun <T : View> findViewsWithType(root: View, type: Class<T>): List<T> {
+        val views = ArrayList<T>()
+        findViewsWithType(root, type, views)
+        return views
+    }
+
+    private fun <T : View> findViewsWithType(view: View, type: Class<T>, views: MutableList<T>) {
+        if (type.isInstance(view)) {
+            views.add(type.cast(view))
+        }
+
+        if (view is ViewGroup) {
+            for (i in 0 until view.childCount) {
+                findViewsWithType(view.getChildAt(i), type, views)
+            }
+        }
+    }
+}
