72563cbcb4baa63e59aba4029c9b1fed5555a6b1
Cameron Ketcham <cketcham@google.com>
2018-04-19 16:14:28 -0400

    Ending point for 101

--- kotlin/shrine/app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/LoginFragment.kt
+++ kotlin/shrine/app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/LoginFragment.kt
@@ -1,10 +1,13 @@
 package com.google.codelabs.mdc.kotlin.shrine
 
 import android.os.Bundle
+import android.text.Editable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.shr_login_fragment.*
+import kotlinx.android.synthetic.main.shr_login_fragment.view.*
 
 /**
  * Fragment representing the login screen for Shrine.
@@ -14,12 +17,33 @@ class LoginFragment : Fragment() {
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-
-        // Snippet from "Navigate to the next Fragment" section goes here.
-
         val view = inflater.inflate(R.layout.shr_login_fragment, container, false)
+
+        // Set an error if the password is less than 8 characters.
+        view.next_button.setOnClickListener {
+            if (!isPasswordValid(password_edit_text.text)) {
+                password_text_input.error = getString(R.string.shr_error_password)
+            } else {
+                password_text_input.error = null // Clear the error
+                (activity as NavigationHost).navigateTo(ProductGridFragment(), false) // Navigate to the next Fragment
+            }
+        }
+
+        // Clear the error once more than 8 characters are typed.
+        view.password_edit_text.setOnKeyListener { _, _, _ ->
+            if (isPasswordValid(password_edit_text.text)) {
+                password_text_input.error = null //Clear the error
+            }
+            false
+        }
         return view
     }
 
-    // "isPasswordValid" from "Navigate to the next Fragment" section method goes here
+    /*
+        In reality, this will have more complex logic including, but not limited to, actual
+        authentication of the username and password.
+     */
+    private fun isPasswordValid(text: Editable?): Boolean {
+        return text != null && text.length >= 8
+    }
 }
