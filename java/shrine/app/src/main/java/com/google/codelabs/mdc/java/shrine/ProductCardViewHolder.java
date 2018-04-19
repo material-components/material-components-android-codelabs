9e0db3164670b423e029816745199fd7a2f3e2fe
Cameron Ketcham <cketcham@google.com>
2018-04-19 16:23:50 -0400

    Ending point for 102

--- java/shrine/app/src/main/java/com/google/codelabs/mdc/java/shrine/ProductCardViewHolder.java
+++ java/shrine/app/src/main/java/com/google/codelabs/mdc/java/shrine/ProductCardViewHolder.java
@@ -1,13 +1,23 @@
 package com.google.codelabs.mdc.java.shrine;
 
 import android.view.View;
+import android.widget.TextView;
+
+import com.android.volley.toolbox.NetworkImageView;
+
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class ProductCardViewHolder extends RecyclerView.ViewHolder {
 
+    public NetworkImageView productImage;
+    public TextView productTitle;
+    public TextView productPrice;
+
     public ProductCardViewHolder(@NonNull View itemView) {
         super(itemView);
-        //TODO: Find and store views from itemView
+        productImage = itemView.findViewById(R.id.product_image);
+        productTitle = itemView.findViewById(R.id.product_title);
+        productPrice = itemView.findViewById(R.id.product_price);
     }
 }
