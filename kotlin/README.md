3075f6deb28be8d978174cf3212a0897f8d170c4
Cameron Ketcham <cketcham@google.com>
2018-04-19 16:23:50 -0400

    Ending point for 102

--- kotlin/README.md
+++ kotlin/README.md
@@ -1,3 +1,3 @@
 # MDC-102 for Material Components for Android (Kotlin)
 
-Contains starter code structure for the MDC-102 Kotlin codelab.
+Contains complete code structure for the MDC-102 Kotlin codelab.
--- kotlin/shrine/app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/ProductCardViewHolder.kt
+++ kotlin/shrine/app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/ProductCardViewHolder.kt
@@ -1,7 +1,14 @@
 package com.google.codelabs.mdc.kotlin.shrine
 
 import android.view.View
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class ProductCardViewHolder(itemView: View) //TODO: Find and store views from itemView
-    : RecyclerView.ViewHolder(itemView)
+import com.android.volley.toolbox.NetworkImageView
+
+class ProductCardViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    var productImage: NetworkImageView = itemView.findViewById(R.id.product_image)
+    var productTitle: TextView = itemView.findViewById(R.id.product_title)
+    var productPrice: TextView = itemView.findViewById(R.id.product_price)
+}
