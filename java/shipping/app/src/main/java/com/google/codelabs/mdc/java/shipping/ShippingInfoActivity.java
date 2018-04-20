54d556910bdc961e353754c5bfc5bf45786a75a6
Cameron Ketcham <cketcham@google.com>
2018-04-20 15:22:54 -0400

    Ending point for 111

--- java/shipping/app/src/main/java/com/google/codelabs/mdc/java/shipping/ShippingInfoActivity.java
+++ java/shipping/app/src/main/java/com/google/codelabs/mdc/java/shipping/ShippingInfoActivity.java
@@ -1,6 +1,12 @@
 package com.google.codelabs.mdc.java.shipping;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.List;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +16,32 @@ public class ShippingInfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.shipping_info_activity);
+
+        View rootView = findViewById(android.R.id.content);
+
+        final List<TextInputLayout> textInputLayouts = Utils.findViewsWithType(
+                rootView, TextInputLayout.class);
+
+        Button button = findViewById(R.id.save_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean noErrors = true;
+                for (TextInputLayout textInputLayout : textInputLayouts) {
+                    String editTextString = textInputLayout.getEditText().getText().toString();
+                    if (editTextString.isEmpty()) {
+                        textInputLayout.setError(getResources().getString(R.string.error_string));
+                        noErrors = false;
+                    } else {
+                        textInputLayout.setError(null);
+                    }
+                }
+
+                if (noErrors) {
+                    // All fields are valid!
+                }
+            }
+        });
     }
+
 }
--- kotlin/shipping/app/src/main/java/com/google/codelabs/mdc/kotlin/shipping/ShippingInfoActivity.kt
+++ kotlin/shipping/app/src/main/java/com/google/codelabs/mdc/kotlin/shipping/ShippingInfoActivity.kt
@@ -1,12 +1,37 @@
 package com.google.codelabs.mdc.kotlin.shipping
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputLayout
+import kotlinx.android.synthetic.main.shipping_info_activity.*
 
 class ShippingInfoActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.shipping_info_activity)
+
+        val rootView = findViewById<View>(android.R.id.content)
+
+        val textInputLayouts = Utils.findViewsWithType(
+                rootView, TextInputLayout::class.java)
+
+        save_button.setOnClickListener {
+            var noErrors = true
+            for (textInputLayout in textInputLayouts) {
+                val editTextString = textInputLayout.editText!!.text.toString()
+                if (editTextString.isEmpty()) {
+                    textInputLayout.error = resources.getString(R.string.error_string)
+                    noErrors = false
+                } else {
+                    textInputLayout.error = null
+                }
+            }
+
+            if (noErrors) {
+                // All fields are valid!
+            }
+        }
     }
 }
