fffa4aaf6458c74cf68490991a7d2b978778f5bf
melaniegoetz <melaniegoetz@google.com>
2019-04-24 17:45:59 -0400

    update dependencies to androidx

--- java/shipping/app/src/androidTest/java/com/google/codelabs/mdc/java/shipping/ExampleInstrumentedTest.java
+++ java/shipping/app/src/androidTest/java/com/google/codelabs/mdc/java/shipping/ExampleInstrumentedTest.java
@@ -1,11 +1,11 @@
 package com.google.codelabs.mdc.java.shipping;
 
 import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import androidx.test.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import static org.junit.Assert.*;
 
